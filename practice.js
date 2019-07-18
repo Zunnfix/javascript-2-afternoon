ed462a5b988d3e09a13d3b8150c70f279a3bed0b
Chase Shields <cwshields2@gmail.com>
2019-07-18 16:06:22 -0500

    completed

--- practice.js
+++ practice.js
@@ -112,20 +112,24 @@ user2.email = 'bryan.smith@devmounta.in'
   Create an empty object called methodCollection.
 */
 
-//Code Here
+let methodCollection = {}
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
-//Code Here
+methodCollection = {
+  alertHello: alert('hello'),
+  logHello: console.log('hello')
+}
 
 /*
   Now call your alertHello and logHello methods.
 */
 
-//Code Here
+methodCollection.alertHello()
+methodCollection.logHello()
 
 ////////// PROBLEM 6 //////////
 
